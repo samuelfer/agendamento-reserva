1fbf549022068bf95eb652789c4986ce001ca86d
sfsantos <sfsantos@tce.pb.gov.br>
2023-08-12 20:56:27 -0300

    Add método para listar imóvel pelo número

--- src/main/java/com/reserva/repository/ImovelRepository.java
+++ src/main/java/com/reserva/repository/ImovelRepository.java
@@ -4,6 +4,9 @@ import com.reserva.model.Imovel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ImovelRepository  extends JpaRepository<Imovel, Long> {
+    Optional<Imovel> findFirstByNumero(String numero);
 }
